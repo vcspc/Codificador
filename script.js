1c9fe28a9da850d6183b9f7618edfa61a7a992f5
vcspc <107441402+vcspc@users.noreply.github.com>
2022-08-18 22:56:59 -0300

    tentativa de converter newMessageCode em texto

--- script.js
+++ script.js
@@ -7,21 +7,29 @@ function start(){
 
     function handButtonClickCode(){
         var textMessage = document.querySelector('#input-message');
-        
+        var codeResult = document.querySelector('#result');
+
+
         var message = textMessage.value;
         var messageCode = [];
-        var newMessageCode = []
-        
+        var newMessageCode = [];
+        var result = [];
+
         for(var cont = 0; cont < message.length; cont++){
              messageCode.push(message.charCodeAt(cont));
            newMessageCode.push(addMessageCode(messageCode[cont], cont));
+           result.push(newMessageCode.fromCharCode(newMessageCode[cont]));
         }
        
+        
+
         // var newMessageCode = messageCode.map(x => x + 3);
         
         console.log(message);
         console.log(messageCode);
         console.log(newMessageCode);
+
+        codeResult.textContent = result;
         
     }
 
