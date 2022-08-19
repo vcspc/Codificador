7afa647b69feb053ec77b9f2e8dddf8e9731f191
vcspc <107441402+vcspc@users.noreply.github.com>
2022-08-18 22:19:08 -0300

    implementação da regra de codificação

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 function start(){
 
     var buttonCode = document.querySelector('#button-code');
-    buttonCode.addEventListener('click', handButtonClick);
-
-    //textMessage.onkeypress = keyPress;
-
-    function handButtonClick(){
-        var textMessage = document.querySelector('#input-message');
-        
-        var message = textMessage.value;
-
-        message.charCodeAt;
-
-    }
+    buttonCode.addEventListener('click', handButtonClickCode);
 
 }
 
-start();
+    function handButtonClickCode(){
+        var textMessage = document.querySelector('#input-message');
+        
+        var message = textMessage.value;
+        var messageCode = [];
+        var someMessageCode = []
+        
+        for(var cont = 0; cont < message.length; cont++){
+             messageCode.push(message.charCodeAt(cont));
+           someMessageCode.push(newMessageCode(messageCode[cont], cont));
+        }
+       
+        // var newMessageCode = messageCode.map(x => x + 3);
+        
+        console.log(message);
+        console.log(messageCode);
+        console.log(someMessageCode);
+        
+    }
 
+    function newMessageCode(number, cont){
+       return number + cont
+    }
+
+start();
